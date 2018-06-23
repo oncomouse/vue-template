c13b507d25f1c123972ae379ea5726c99c507cc7
oncomouse <oncomouse@gmail.com>
2018-06-23 14:48:30 -0500

    One miss with Vuex

--- template/app/index.js
+++ template/app/index.js
@@ -4,10 +4,14 @@ import createStore from './store';
 import App from './App.vue';
 
 const router = createRouter();
+{{#vuex}}
 const store = createStore();
+{{/vuex}}
 const app = new Vue({
   router,
+  {{#vuex}}
   store,
+  {{/vuex}}
   components: { App },
   template: '<App/>',
 });
