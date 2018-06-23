4b9ee2c1f25d5366ec878647dbd839c339884c94
oncomouse <oncomouse@gmail.com>
2018-06-23 14:39:31 -0500

    Added installation of dependencies

--- meta.js
+++ meta.js
@@ -30,9 +30,50 @@ module.exports = {
       type: 'confirm',
       message: 'Install Vuex (state manager)?',
     },
+    autoInstall: {
+      type: 'list',
+      message: 'Should we run `npm install` for you after the project has been created? (recommended)',
+      choices: [
+        {
+          name: 'Yes, use NPM',
+          value: 'npm',
+          short: 'npm',
+        },
+        {
+          name: 'Yes, use Yarn',
+          value: 'yarn',
+          short: 'yarn',
+        },
+        {
+          name: 'No, I will handle that myself',
+          value: false,
+          short: 'no',
+        },
+      ],
+    },
   },
   filters: {
     'app/store/**/*': 'vuex',
     'test/unit/specs/store/**/*': 'vuex',
+  },
+  complete: function(data, { chalk }) {
+    const { green } = chalk;
+
+    const cwd = path.join(process.cwd(), data.inPlace ? '' : data.destDirName);
+
+    if (data.autoInstall) {
+      installDependencies(cwd, data.autoInstall, green);
+        /*.then(() => {
+          return runLintFix(cwd, data, green)
+        })*/
+        .then(() => {
+          printMessage(data, chalk);
+        })
+        .catch(e => {
+          console.log(chalk.red('Error:'), e);
+        })
+    } else {
+      printMessage(data, chalk);
+    }
   },
 };
---
+++ utils/index.js
@@ -0,0 +1,59 @@
+const { spawn } = require('child_process');
+
+/**
+ * Spawns a child process and runs the specified command
+ * By default, runs in the CWD and inherits stdio
+ * Options are the same as node's child_process.spawn
+ * @param {string} cmd
+ * @param {array<string>} args
+ * @param {object} options
+ */
+function runCommand(cmd, args, options) {
+  return new Promise((resolve, reject) => {
+    const proc = spawn(
+      cmd,
+      args,
+      Object.assign(
+        {
+          cwd: process.cwd(),
+          stdio: 'inherit',
+          shell: true,
+        },
+        options
+      )
+    );
+
+    proc.on('exit', () => {
+      resolve()
+    });
+  });
+};
+
+exports.installDependencies = function installDependencies(
+  cwd,
+  executable = 'npm',
+  color
+) {
+  console.log(`\n\n# ${color('Installing project dependencies ...')}`);
+  console.log('# ========================\n');
+  return runCommand(executable, ['install'], {
+    cwd,
+  });
+};
+/**
+ * Prints the final message with instructions of necessary next steps.
+ * @param {Object} data Data from questionnaire.
+ */
+exports.printMessage = function printMessage(data, { green, yellow }) {
+  const message = `
+# ${green('Project initialization finished!')}
+# ========================
+To get started:
+  ${yellow(
+    `${data.inPlace ? '' : `cd ${data.destDirName}\n  `}${installMsg(
+      data
+    )}${lintMsg(data)}npm run dev`
+  )}
+`
+  console.log(message)
+}
